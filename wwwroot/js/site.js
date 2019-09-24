8bcd9c489dd11a69feda6e36abb60ca2e98587b3
Simon Moe Sørensen <moe.simon@gmail.com>
2019-09-24 16:24:56 -0700

    Added more styling Started login and signup form implementation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+(function (window) {
+    "use strict";
+
+})(window);
+
+$('#login-modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var recipient = button.data('type');
+
+    var modal = $('#login-modal');
+    modal.find('.modal-title').text(recipient);
+    modal.find('[id=modal-button]').text(recipient);
+
+    if (recipient === "Signup") {
+        modal.find('[id=login-form]').hide();
+        modal.find('[id=signup-form]').show();   
+    } else {
+        modal.find('[id=signup-form]').hide();
+        modal.find('[id=login-form]').show();
+    }
+});
