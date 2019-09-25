664c7ef38c294c09ab1dce1dc8e75f9d1c24bb86
Simon Moe Sørensen <moe.simon@gmail.com>
2019-09-24 17:12:33 -0700

    Added formhandlers and some UI tweaking

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,9 +3,25 @@
 
 // Write your JavaScript code.
 
-
+var modal = $('#login-modal');
 (function (window) {
     "use strict";
+    var LOGIN_FORM_SELECTOR = "[id=\"login-form\"]";
+    var SIGNUP_FORM_SELECTOR = "[id=\"signup-form\"]";
+
+    var App = window.App;
+    var FormHandler = App.FormHandler;
+    var login_form = new FormHandler(LOGIN_FORM_SELECTOR);
+    var signup_form = new FormHandler(SIGNUP_FORM_SELECTOR);
+
+    login_form.addSubmitHandler(function (data) {
+        console.log("Login received data: " + data);
+    });
+
+    signup_form.addSubmitHandler(function (data) {
+        console.log("Signup received data: " + data);
+    });
+
 
 })(window);
 
@@ -13,9 +29,8 @@ $('#login-modal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
     var recipient = button.data('type');
 
-    var modal = $('#login-modal');
+    
     modal.find('.modal-title').text(recipient);
-    modal.find('[id=modal-button]').text(recipient);
 
     if (recipient === "Signup") {
         modal.find('[id=login-form]').hide();
