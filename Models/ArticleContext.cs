6bfb2b906c9c793c83891bf0a1e31d9d0d00abe1
Daniel <32684450+Daniel-Ronson@users.noreply.github.com>
2019-09-30 01:08:36 -0700

    Added database connection
    {localhost}/Articles
    -return lists of articles

---
+++ Models/ArticleContext.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Models
+{
+    public class ArticleContext
+    {
+        public string ConnectionString { get; set; }
+
+        public ArticleContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection getConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+        public List<Article> ListArticles()
+        {
+            List<Article> LArticle = new List<Article>();
+
+            using (MySqlConnection con = getConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand("ListArticles", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+
+                con.Open(); //open db connection
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                //while loop
+                while (rdr.Read())
+                {
+                    Article w = new Article();
+                    w.ID = Convert.ToInt32(rdr["articleid"]);
+                    w.Title = rdr["title"].ToString();
+                    w.Url = rdr["url"].ToString();
+                    w.Date = rdr["date"].ToString();
+                    w.WebsiteId = rdr["websiteid"].ToString();
+                    w.Genre = rdr["genreid"].ToString();
+
+                    LArticle.Add(w);
+                }
+
+                con.Close();
+            }
+            return LArticle;
+        }
+    }
+}
