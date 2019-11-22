bfd13e4a32038728c883c7864420cb69ed3436f7
Daniel <32684450+Daniel-Ronson@users.noreply.github.com>
2019-11-21 23:31:16 -0500

    added UUID to cookie

--- Controllers/CookiesController.cs
+++ Controllers/CookiesController.cs
@@ -15,20 +15,21 @@ namespace News.Controllers
         {
             if (!HttpContext.Request.Cookies.ContainsKey("session_id"))
             {
+                string SessionId = Guid.NewGuid().ToString();
                 CookieOptions cookieOptions = new CookieOptions
                 {
-                    Expires = new DateTimeOffset(DateTime.Now.AddDays(2))
+                    Expires = new DateTimeOffset(DateTime.Now.AddDays(1))
                 };
                 HttpContext.Response.Cookies.Append("date_of_creation", DateTime.Now.ToString(),cookieOptions); //set cookie
-                HttpContext.Response.Cookies.Append("session_id", "12345", cookieOptions); //set cookie
-                return Content("Welcome, new visitor!" + HttpContext.Request.Cookies["session_id"]);
+                HttpContext.Response.Cookies.Append("session_id", SessionId, cookieOptions); //set cookie
+                return Content( "Welcome, new visitor! Session Id: " + HttpContext.Request.Cookies["session_id"]);
             }
             else
             {
                 DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["date_of_creation"]);
                 string sessionid = HttpContext.Request.Cookies["session_id"];
                 //cookieOptions.Expires = DateTime.Now.AddDays(2);
-                return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString()+ " id: " + sessionid);
+                return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString()+ " Sessionid: " + HttpContext.Request.Cookies["session_id"]);
             }
         }
     }
